6006427e7a03024dc0ae94084fcb4f8ff9ebb0f9
Kristina Pelevina <christya_13.07@mail.ru>
2020-07-15 18:45:34 +0600

    clsses changed+

--- BmiService.java
+++ BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public double calculate (double kg) {
-
-        double sm = 151;
+    public double calculate (double kg, double sm) {
 
         double bmi = (kg / ( sm * sm )) * 10_000;
 
--- Main.java
+++ Main.java
@@ -2,15 +2,9 @@ public class Main {
     public static void main(String[] args) {
         BmiService bmiService = new BmiService();
 
-        double bmi = bmiService.calculate(47);
+        double bmi = bmiService.calculate(47, 151);
 
         System.out.println(bmi);
 
-//        double kg = 47;
-//        double sm = 151;
-//
-//        double bmi = (kg / ( sm * sm )) * 10_000;
-//
-//        System.out.println(bmi);
-    }
+   }
 }
